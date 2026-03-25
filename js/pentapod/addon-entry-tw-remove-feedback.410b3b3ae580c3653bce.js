c84dff699448d3531fbd1a0909db2623bc38e35c
BrickPlex <144573025+BrickPlex@users.noreply.github.com>
2026-03-25 11:35:15 -0400

    release
    this is just to host WarpSpeed.

---
+++ js/pentapod/addon-entry-tw-remove-feedback.410b3b3ae580c3653bce.js
@@ -0,0 +1,1 @@
+(window.webpackJsonpGUI=window.webpackJsonpGUI||[]).push([[60],{1548:function(n,e,s){"use strict";s.r(e),s.d(e,"resources",function(){return o});var c=s(1549);const o={"remove-feedback.css":s.n(c).a}},1549:function(n,e,s){(n.exports=s(10)(!1)).push([n.i,'a[class^="menu-bar_feedback-link_"] {\n  display: none;\n}\n',""])}}]);
